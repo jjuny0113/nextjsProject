b46a04ae2e7bbc23e597b9d5708a22a6ae1a2f34
junny <jjun@earlysloth.com>
2022-08-02 01:19:05 +0900

    profile stream ui 완성

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 space-y-5 py-16">
       <div>
         <label className="w-full cursor-pointer flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500">
           <svg
@@ -22,7 +22,23 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="mb-5">
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700 mt-7"
           htmlFor="price"
@@ -45,12 +61,16 @@ const Upload: NextPage = () => {
         </div>
       </div>
       <div>
-        <label className="mb-1 block text-sm font-medium text-gray-700 mt-7">
+        <label
+          htmlFor="description"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
           Description
         </label>
 
         <textarea
-          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md broder-gray-300 focus:border-orange-500"
+          id="description"
+          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md broder-gray-300 focus:border-orange-500 resize-none"
           rows={4}
         />
       </div>
